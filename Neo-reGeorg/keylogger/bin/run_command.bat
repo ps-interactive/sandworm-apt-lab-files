b6ccb7a46be8e95c03d2c29ff11aa18ad08ff4a5
Matt Lloyd Davies <103773518+Turzum@users.noreply.github.com>
2024-09-25 18:11:49 +0100

    Delete Neo-reGeorg/keylogger directory

--- Neo-reGeorg/keylogger/bin/run_command.bat
+++
@@ -1,2 +0,0 @@
-:: https://superuser.com/questions/198525/how-can-i-execute-a-windows-command-line-in-background
-START /B "" SetWindowsHookEx-Keylogger.exe -o keylogger_output.txt
--- Neo-reGeorg/keylogger/make.bat
+++
@@ -1,1 +0,0 @@
-g++ -Wall src/SetWindowsHookEx-Keylogger.cpp -o bin/SetWindowsHookEx-Keylogger.exe
